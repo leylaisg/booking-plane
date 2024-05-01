f188b9b9eb3d15858427a05cb137439d61e9bd24
Vusal Atashov <156101309+Vusal-Atashov@users.noreply.github.com>
2024-05-01 16:33:44 +0400

    booking app

---
+++ src/main/java/Booking.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class Booking {
+    private String id;
+    private String flightId;
+    private List<String> passengerNames;
+
+}
---
+++ src/main/java/dao/FlightsDao.java
@@ -0,0 +1,6 @@
+package dao;
+
+public class FlightsDao {
+    public static final String resourceBooking ="src/main/java/resource/";
+
+}
