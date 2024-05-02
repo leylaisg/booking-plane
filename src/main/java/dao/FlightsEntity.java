80d3bbaeb642d8c20b2c1e685e87b1598c1cb7bf
leylaisg <leyla.isgandarli95@gmail.com>
2024-05-02 16:05:58 +0400

    FlightsEntity

--- src/main/java/dao/FlightsEntity.java
+++ src/main/java/dao/FlightsEntity.java
@@ -1,9 +1,60 @@
 package dao;
 
+import java.util.Objects;
+
 public class FlightsEntity {
-    private String id;
+    public static long MAX_ID = 0;
+    private final long id;
     private String destination;
     private String date;
     private int numOfSeats;
 
+    public FlightsEntity(String destination, String date, int numOfSeats) {
+        this.id = ++MAX_ID;
+        this.destination = destination;
+        this.date = date;
+        this.numOfSeats = numOfSeats;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getNumOfSeats() {
+        return numOfSeats;
+    }
+
+    public void setNumOfSeats(int numOfSeats) {
+        this.numOfSeats = numOfSeats;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FlightsEntity that = (FlightsEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("{id='%s', destination='%s', date='%s', numOfSeats=%d}", id, destination, date, numOfSeats);
+    }
 }
